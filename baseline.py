475a2b5838e4d49fe546be7d22a89cdc5401c614
Luca Demma <demma.luca@gmail.com>
2023-01-20 12:51:42 +0100

    ERROR emissionProb read too much time

---
+++ baseline.py
@@ -0,0 +1,34 @@
+import constants
+from helpers import read_from_file
+from math import log
+from constants import NER_TAGS
+from conllu import TokenList
+
+
+# sentence as a list of strings
+def baseline_decode(sentence_list):
+	print(sentence_list)
+
+	emissions_probabilities = read_from_file('emissions_probabilities')
+
+	result_tags = []
+
+	for word in sentence_list:
+		if word in emissions_probabilities:
+			temp_prob = constants.MIN_FLOAT
+			temp_tag = ""
+			for tag in NER_TAGS:
+				if log(emissions_probabilities[word][tag]) > temp_prob:
+					temp_prob = log(emissions_probabilities[word][tag])
+					temp_tag = tag
+			result_tags.append(temp_tag)
+		else:
+			result_tags.append("B-MISC")
+
+	result = TokenList([])
+
+	for index, word in enumerate(sentence_list):
+		# populate TokenList
+		result.append({'id': index, 'form': word, 'tag': result_tags[index]})
+
+	return result
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from helpers import from_conllu_to_sentences, string_to_csv
 import constants
 from decode import decode
+from baseline import baseline_decode
 from pqdm.processes import pqdm
 
 # Read Sentences from conllu test file
@@ -19,7 +20,9 @@ def decode_sentence(sentence):
 	word_list = []
 	for word in sentence:
 		word_list.append(word['form'])
-	sentence_decoded = decode(word_list)
+
+		#sentence_decoded = baseline_decode(word_list)
+		sentence_decoded = decode(word_list)
 
 	csv_sentence = ""
 	# Creating the csv for result comparison
@@ -37,10 +40,12 @@ def decode_sentence(sentence):
 csv_sentences = pqdm(sentences, decode_sentence, n_jobs=constants.NUM_CORES, desc="DECODING...")
 
 
+# Write the result to a CSV
 results_csv = 'ID\tWord\tTAG_Test\tTAG_Calculated\tIS_Correct\n'
 
 for s in csv_sentences:
 	results_csv += s
 
-string_to_csv("results_comparison", results_csv)
+	#string_to_csv("BASELINE_results_comparison", results_csv)
+	string_to_csv("results_comparison", results_csv)
 
